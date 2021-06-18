df5d52204055ac2942235105c5fedb48576e4ced
colcrunch <rhartnett35@gmail.com>
2021-06-18 01:37:27 -0400

    Add initial migration and fix postgres connection url.

--- settings.py
+++ settings.py
@@ -12,19 +12,23 @@ from tomlkit import loads
 # Load the bot config.
 conf = False
 if path.exists('config.toml'):
+    conf = True
     with open("config.toml", "r") as c:
         config = loads(c.read())
 
-db_set = config['db']
-TORTOISE_ORM = {
-    "connections": {
-        "default": f"postgres://{db_set['host']}:{db_set['port']}/{db_set['db_name']}?user={db_set['user']}"
-                   f"&password={db_set['pass']}"
-    },
-    "apps": {
-        "models": {
-            "models": ['aerich.models'],
-            "default_connection": "default",
+if conf:
+    db_set = config['db']
+    TORTOISE_ORM = {
+        "connections": {
+            "default": f"postgres://"
+                       f"{db_set['user']}:{db_set['pass']}@{db_set['host']}:{db_set['port']}/{db_set['db_name']}"
+        },
+        "apps": {
+            "models": {
+                "models": ['aerich.models'],
+                "default_connection": "default",
+            }
         }
     }
-}
+else:
+    TORTOISE_ORM = None
