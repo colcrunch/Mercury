cd71d747e52f791b58778b0e42a434a46cf428ef
colcrunch <rhartnett35@gmail.com>
2021-08-21 20:51:20 -0400

    Expand admin commands and core cog.

---
+++ utils/checks.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+
+def guild_owner():
+    """
+    Checks if a user is the owner of a guild.
+    :return:
+    """
+    async def predicate(ctx):
+        if type(ctx.channel) is discord.DMChannel:
+            return False
+        elif await ctx.bot.is_owner(ctx.author):
+            return True
+        elif ctx.author is ctx.guild.owner:
+            return True
+    return commands.check(predicate)
