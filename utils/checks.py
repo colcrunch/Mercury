57ba2afc83906a50be1c7fbde3ce9e0b3481f29e
colcrunch <rhartnett35@gmail.com>
2021-08-26 17:17:41 -0400

    Add `is_admin` check to see if the user is either an owner or is in the designated botadminrole.

--- utils/checks.py
+++ utils/checks.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+from cogs.core.models import BotAdminRole
+
+from .loggers import get_logger
+
+logger = get_logger(__name__)
 
 
 def guild_owner():
@@ -14,4 +19,31 @@ def guild_owner():
             return True
         elif ctx.author is ctx.guild.owner:
             return True
+    return commands.check(predicate)
+
+
+def is_admin():
+    """
+    Checks if the user is a bot admin.
+    :return:
+    """
+    async def predicate(ctx):
+        if type(ctx.channel) is discord.DMChannel:
+            return False
+        elif await ctx.bot.is_owner(ctx.author):
+            return True
+        elif ctx.author is ctx.guild.owner:
+            return True
+        admin_role_obj = await BotAdminRole.filter(pk=ctx.guild.id).first()
+        if admin_role_obj is None:
+            return False
+
+        # Get Role
+        admin_role_id = admin_role_obj.role_id
+        admin_role = ctx.guild.get_role(admin_role_id)
+        user_roles = ctx.author.roles
+        if admin_role in user_roles:
+            return True
+        else:
+            return False
     return commands.check(predicate)
