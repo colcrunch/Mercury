02af957b81f8586eabccbb4266c9cfcf443e40e0
colcrunch <rhartnett35@gmail.com>
2022-01-23 19:34:04 -0500

    Add method to format timedelta objects to string.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 import async_timeout
+from datetime import timedelta
 
 
 async def get_json(session, url, user_agent=None):
@@ -9,3 +10,31 @@ async def get_json(session, url, user_agent=None):
             resp = await response.json()
             resp_code = response.status
             return {'resp': resp, 'code': resp_code}
+
+
+def strftdelta(tdelta: timedelta) -> str:
+    """
+    Converts a timedelta object to a string.
+    :param tdelta:
+    :return:
+    """
+    d = dict(days=tdelta.days)
+    if d['days'] >= 365:
+        d['yrs'], rem = divmod(d['days'], 365)
+        d['mos'], rem = divmod(rem, 30)
+        d['days'] = rem
+    d['hrs'], rem = divmod(tdelta.seconds, 3600)
+    d['min'], d['sec'] = divmod(rem, 60)
+
+    if 'yrs' in d:
+        fmt = '{yrs}y {mos}m {days}d {hrs}h {min}m'
+    elif d['min'] == 0:
+        fmt = '{sec}s'
+    elif d['hrs'] == 0:
+        fmt = '{min}m {sec}s'
+    elif d['days'] == 0:
+        fmt = '{hrs}h {min}m {sec}s'
+    else:
+        fmt = '{days}d {hrs}h {min}m {sec}s'
+
+    return fmt.format(**d)
